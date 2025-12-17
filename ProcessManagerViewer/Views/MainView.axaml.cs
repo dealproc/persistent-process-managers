62348f1f948c10b07d27e5982fc3832be7dcd68a
Richard Bennett <inbox@richardbennett.dev>
2025-12-17 11:52:41 -0500

    Project shell.
    This project is using Aurelia so it can be ran on windows/linux/macos/web to be more accessible for any developer curious about the technique.

---
+++ ProcessManagerViewer/Views/MainView.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+
+namespace ProcessManagerViewer.Views;
+
+public partial class MainView : UserControl {
+    public MainView() {
+        InitializeComponent();
+    }
+}
---
+++ ProcessManagerViewer/Views/MainWindow.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+
+namespace ProcessManagerViewer.Views;
+
+public partial class MainWindow : Window {
+    public MainWindow() {
+        InitializeComponent();
+    }
+}
